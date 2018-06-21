4bfdb4ce2e980a61569145b2afa933fdac93a5b7
Junior Oliveira <arojunior@gmail.com>
2018-06-20 23:14:53 -0300

    add spring boot boilerplate

---
+++ upvotes/src/main/java/com/arojunior/upvotes/UpvotesApplication.java
@@ -0,0 +1,12 @@
+package com.arojunior.upvotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpvotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpvotesApplication.class, args);
+	}
+}
---
+++ upvotes/src/test/java/com/arojunior/upvotes/UpvotesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.arojunior.upvotes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UpvotesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
